41d05088b93d51922760c93a432ba6a16eb63c65
AkiraNyx <Akira_Nyx@outlook.com>
2026-04-15 22:55:26 +0800

    修复: 服务头文件添加 winioctl.h 和 d3d9.h 解决 include 顺序问题

--- service/service.h
+++ service/service.h
@@ -6,6 +6,8 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <winioctl.h>
+#include <d3d9.h>
 #include <dxva2api.h>
 #include <highlevelmonitorconfigurationapi.h>
 #include <physicalmonitorenumerationapi.h>
